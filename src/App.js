5d365bf660d8565e94776a4b60089984d66ed064
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-05 22:54:31 +0100

    chore: fetch api and set listloading state.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import List from './components/List/List';
+import withListLoading  from './components/withListLoading/withListLoading';
+import './css/App.css';
+
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [q, setQ] = useState('');
+
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    templates: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    fetch('https://front-end-task-dot-fpls-dev.uc.r.appspot.com/api/v1/public/task_templates')
+    .then((response) => response.json())
+    .then((templates) => {
+      setAppState({ loading: false, templates: templates });
+    });
+  }, [setAppState]);
+
   return (
     <div>
-      <h1> Form Template Search In Progress... </h1>
+      <div> search and filter </div>
+      <div>
+        Tada! Get startd with a free template. Can't find
+        what you are looking for? Search from the 
+        1000+ available templates.
+      </div>
+      <div>
+        <ListLoading isLoading={appState.loading} templates={appState.templates}/>
+      </div>
     </div>
   )
 }
