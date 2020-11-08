c818f216a7d2e4c5c5fd65895d384dd0a86cbb1c
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-08 23:21:28 +0100

    chore: import templates.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import List from './components/List/List';
-import withListLoading  from './components/withListLoading/withListLoading';
+import React from 'react';
 import './css/App.css';
-
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
+import Templates from './components/Templates/Templates';
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [q, setQ] = useState('');
-
-  const ListLoading = withListLoading(List);
-  const [appState, setAppState] = useState({
-    loading: false,
-    templates: null,
-  });
-
-  useEffect(() => {
-    setAppState({ loading: true });
-    fetch('https://front-end-task-dot-fpls-dev.uc.r.appspot.com/api/v1/public/task_templates')
-    .then((response) => response.json())
-    .then((templates) => {
-      setAppState({ loading: false, templates: templates });
-    });
-  }, [setAppState]);
 
   return (
-    <div>
-      <div> search and filter </div>
-      <div>
-        Tada! Get startd with a free template. Can't find
-        what you are looking for? Search from the 
-        1000+ available templates.
-      </div>
-      <div>
-        <ListLoading isLoading={appState.loading} templates={appState.templates}/>
-      </div>
-    </div>
+    <Templates/>
   )
 }
 
