0bf27613b03f92914c9a437e12e67801f05b53d3
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-08 23:23:36 +0100

    Create sortByNameAndCategory.js

---
+++ src/components/Templates/extra/sortByNameAndCategory.js
@@ -0,0 +1,39 @@
+//First sort by category then sort by name
+
+export const sortByNameAZ = arr => {
+    // eslint-disable-next-line
+    return arr.sort((item1, item2) => {
+      /*if (item1.category > item2.category) return 1;
+      if (item1.category < item2.category) return -1;*/
+      if (item1.name > item2.name) return 1;
+      if (item1.name < item2.name) return -1;
+    });
+  };
+  
+  export const sortByNameZA = arr => {
+    // eslint-disable-next-line
+    return arr.sort((item1, item2) => {
+      /*if (item1.category > item2.category) return 1;
+      if (item1.category < item2.category) return -1;*/
+      if (item1.name > item2.name) return -1;
+      if (item1.name < item2.name) return 1;
+    });
+  };
+
+  export const sortByDateAZ = arr => {
+    // eslint-disable-next-line
+    return arr.sort((item1, item2) => {
+     
+      if (item1.created > item2.created) return 1;
+      if (item1.created < item2.created) return -1;
+    });
+  };
+  
+  export const sortByDateZA = arr => {
+    // eslint-disable-next-line
+    return arr.sort((item1, item2) => {
+      
+      if (item1.created > item2.created) return -1;
+      if (item1.created < item2.created) return 1;
+    });
+  };
