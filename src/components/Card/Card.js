de20d93c657035f8b67d7fbf8104cc0e8131b576
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2020-11-11 21:56:56 +0100

    chore: edit class names.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,14 +3,14 @@ import '../../css/Card.css';
 
 function Card(props) {
   return (
-    <div className="person">
-      <div className="person-details">
-        <div className="person-name">
+    <div className="card">
+      <div className="card-details">
+        <div className="card-name">
           <p>Name: {props.name}</p>
           <p>Description: {props.description}</p>
           <p>Link: {props.link}</p>
           <p>Created: {props.created}</p>
-          <div className='person-category'> Category: {props.category} </div>
+          <div className='card-category'> Category: {props.category} </div>
         </div>
       </div>
     </div>
